7a4f268f9d5dd0557292e9660566f0b29ac91307
Edenn <deozza@gmail.com>
2019-07-06 23:12:50 +0200

    fix typos

--- tests/User/UserControllerTest.php
+++ tests/User/UserControllerTest.php
@@ -37,24 +37,24 @@ class UserControllerTest extends TestAsserter
       ["kind" => "unit", "test" => ['method'=> 'PATCH'  , 'url' => 'api/user/00400000-0000-5000-a000-000000000000', 'token' => 'token_userActive' , 'status' => 403] ],
 
       ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postExistingEmail'   , 'out' => 'postedExistingEmail'   ] ],
-      ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postMissingDotEmail' , 'out' => 'postedMissingDotEmail' ] ]
-      ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postMissingAtEmail'  , 'out' => 'postedMissingAtEmail'  ] ]
+      ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postMissingDotEmail' , 'out' => 'postedMissingDotEmail' ] ],
+      ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postMissingAtEmail'  , 'out' => 'postedMissingAtEmail'  ] ],
       ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postExistingLogin'   , 'out' => 'postedExistingLogin'   ] ],
       ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postInvalidPassword' , 'out' => 'postedInvalidPassword' ] ],
       ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postExtraField'      , 'out' => 'postedExtraField'      ] ],
       ["kind" => "unit", "test" => ['method'=> 'POST'   , 'url' => 'api/user' , 'status' => 400, 'in' => 'postMissingField'    , 'out' => 'postedMissingField'    ] ],
 
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchExistingEmail'   , 'out' => 'patchedExistingEmail'   ] ],
-      ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchMissingDotEmail' , 'out' => 'patchedMissingDotEmail' ] ]
-      ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchMissingAtEmail'  , 'out' => 'patchedMissingAtEmail'  ] ]
+      ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchMissingDotEmail' , 'out' => 'patchedMissingDotEmail' ] ],
+      ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchMissingAtEmail'  , 'out' => 'patchedMissingAtEmail'  ] ],
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchExistingLogin'   , 'out' => 'patchedExistingLogin'   ] ],
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchInvalidPassword' , 'out' => 'patchedInvalidPassword' ] ],
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchExtraField'      , 'out' => 'patchedExtraField'      ] ],
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/current' , 'status' => 400, 'in' => 'patchMissingField'    , 'out' => 'patchedMissingField'    ] ],
 
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/00400000-0000-5000-a000-000000000000' , 'status' => 400, 'in' => 'patchExistingEmailAdmin'   , 'out' => 'patchedExistingEmailAdmin'   ] ],
-      ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/00400000-0000-5000-a000-000000000000' , 'status' => 400, 'in' => 'patchMissingDotEmailAdmin' , 'out' => 'patchedMissingDotEmailAdmin' ] ]
-      ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/00400000-0000-5000-a000-000000000000' , 'status' => 400, 'in' => 'patchMissingAtEmailAdmin'  , 'out' => 'patchedMissingAtEmailAdmin'  ] ]
+      ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/00400000-0000-5000-a000-000000000000' , 'status' => 400, 'in' => 'patchMissingDotEmailAdmin' , 'out' => 'patchedMissingDotEmailAdmin' ] ],
+      ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/00400000-0000-5000-a000-000000000000' , 'status' => 400, 'in' => 'patchMissingAtEmailAdmin'  , 'out' => 'patchedMissingAtEmailAdmin'  ] ],
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/00400000-0000-5000-a000-000000000000' , 'status' => 400, 'in' => 'patchExistingLoginAdmin'   , 'out' => 'patchedExistingLoginAdmin'   ] ],
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/00400000-0000-5000-a000-000000000000' , 'status' => 400, 'in' => 'patchInvalidPasswordAdmin' , 'out' => 'patchedInvalidPasswordAdmin' ] ],
       ["kind" => "unit", "test" => ['method'=> 'PATCH'   , 'url' => 'api/user/00400000-0000-5000-a000-000000000000' , 'status' => 400, 'in' => 'patchExtraFieldAdmin'      , 'out' => 'patchedExtraFieldAdmin'      ] ],
