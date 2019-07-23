cca7c95d1cdc615892324c059924d3fd401b884a
Edenn <deozza@gmail.com>
2019-07-23 11:07:25 +0200

    fix email assertion in forms

--- src/Form/user/PatchCurrentUserType.php
+++ src/Form/user/PatchCurrentUserType.php
@@ -6,15 +6,25 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class PatchCurrentUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $option)
     {
         $builder->add('username')
-            ->add('email')
+            ->add('email',TextType::class, [
+                "constraints"=>[
+                    new Assert\Email([
+                        "checkMX"=>true,
+                        "message" => 'The email "{{ value }}" is not a valid email.'
+                    ])
+                ]
+            ])
             ->add('newPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => ['label' => 'Password'],
--- src/Form/user/PostUserType.php
+++ src/Form/user/PostUserType.php
@@ -6,15 +6,25 @@ use App\Entity\SubscribeUser;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class PostUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $option)
     {
         $builder->add('username')
-            ->add('email')
+            ->add('email', TextType::class, [
+                "constraints"=>[
+                    new Assert\Email([
+                        "checkMX"=>true,
+                        "message" => 'The email "{{ value }}" is not a valid email.'
+                    ])
+                ]
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => ['label' => 'Password'],
