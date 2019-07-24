654c9aab2dcd084a5c3bc6ab4466c21ef11c313a
A.MEGEVANT <antoine.megevant@gmail.com>
2019-07-24 14:52:51 +0200

    PasswordControllerTest modified

--- tests/Password/PasswordControllerTest.php
+++ tests/Password/PasswordControllerTest.php
@@ -26,21 +26,27 @@ class PasswordControllerTest extends TestAsserter
   {
     return
     [
-      ["kind" => "unit", "test" => ['method'=> 'PUT'    , 'url' => 'api/password', 'status' => 405] ],
-      ["kind" => "unit", "test" => ['method'=> 'DELETE' , 'url' => 'api/password', 'status' => 405] ],
-      ["kind" => "unit", "test" => ['method'=> 'PATCH'  , 'url' => 'api/password', 'status' => 401] ],
-      ["kind" => "unit", "test" => ['method'=> 'GET'    , 'url' => 'api/password', 'status' => 401] ],
+      ["kind" => "unit", "test" => ['method'=> 'PUT'    , 'url' => 'api/password/reset/request', 'status' => 405] ],
+      ["kind" => "unit", "test" => ['method'=> 'DELETE' , 'url' => 'api/password/reset/request', 'status' => 405] ],
+      ["kind" => "unit", "test" => ['method'=> 'PATCH'  , 'url' => 'api/password/reset/request', 'status' => 405] ],
+      ["kind" => "unit", "test" => ['method'=> 'GET'    , 'url' => 'api/password/reset/request', 'status' => 405] ],
+      #api/password/reset (tout bloquer sauf patch)
 
-      ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password' , 'status' => 400, 'in' => 'postMissingDotEmail' , 'out' => 'postedMissingDotEmail' ] ],
-      ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password' , 'status' => 400, 'in' => 'postMissingAtEmail'  , 'out' => 'postedMissingAtEmail'  ] ],
       ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password' , 'status' => 400, 'in' => 'postExtraField'      , 'out' => 'postedExtraField'      ] ],
       ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password' , 'status' => 400, 'in' => 'postMissingField'    , 'out' => 'postedMissingField'    ] ],
+      #checker si truc dedans
 
       ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password', 'status' => 201, 'in' => 'postEmailOK', 'out' => 'postedEmailOk'] ],
       ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password', 'status' => 201, 'in' => 'postEmptyOk', 'out' => 'postedEmptyOk'] ],
 
-      ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password/token_userActive', 'status' => 200, 'in' => 'postTokenOK', 'out' => 'postedTokenOk'] ],
-      ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password/token_Broken'    , 'status' => 400, 'in' => 'postWrongToken', 'out' => 'postedWrongToken'] ],
+      ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => 'api/password/reset', 'status' => 400, 'in' => 'postWrongToken', 'out' => 'postedWrongToken'] ],# + password et token dans payload
+      #token expiré
+      #extra
+      #missing
+      #checker user invalid
+      #type token invalide
+      #mot de passe invalide
+
     ];
   }
 }
