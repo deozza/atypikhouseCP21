4f2ca33997b9bb1002c74bf8812a5114d9bea240
Edenn <deozza@gmail.com>
2019-07-25 10:30:34 +0200

    basic tests for estate are done. Need test again with events

--- tests/Estate/EstateControllerTest.php
+++ tests/Estate/EstateControllerTest.php
@@ -26,25 +26,33 @@ class EstateControllerTest extends TestAsserter
     {
         return
             [
-                ["kind" => "unit", "test" => ['method'=> 'GET', 'url' => 'api/entities/estate', 'status' => 200, 'out' => "getEstatesAsNotLogged"] ],
-                ["kind" => "unit", "test" => ['method'=> 'GET', 'url' => 'api/entities/estate', 'token'=>'token_userAdmin', 'status' => 200, 'out' => "getEstatesAsAdmin"] ],
-                ["kind" => "unit", "test" => ['method'=> 'GET', 'url' => 'api/entities/estate', 'token'=>'token_userActive', 'status' => 200, 'out' => "getEstatesAsUser"] ],
-                ["kind" => "unit", "test" => ['method'=> 'GET', 'url' => "api/entities/estate?filterBy[equal.validationState]=posted", 'token'=>'token_userActive', 'status' => 200, 'out' => "getEstatesAsUserFiltered"] ],
+                ["kind" => "unit", "test" => ['method'=> 'GET'   , 'url' => 'api/entities/estate'                                                                       , 'status' => 200, 'out' => "getEstatesAsNotLogged"] ],
+                ["kind" => "unit", "test" => ['method'=> 'GET'   , 'url' => 'api/entities/estate'                                       , 'token'=>'token_userAdmin'    , 'status' => 200, 'out' => "getEstatesAsAdmin"] ],
+                ["kind" => "unit", "test" => ['method'=> 'GET'   , 'url' => 'api/entities/estate'                                       , 'token'=>'token_userActive'   , 'status' => 200, 'out' => "getEstatesAsUser"] ],
+                ["kind" => "unit", "test" => ['method'=> 'GET'   , 'url' => "api/entities/estate?filterBy[equal.validationState]=posted", 'token'=>'token_userActive'   , 'status' => 200, 'out' => "getEstatesAsUserFiltered"] ],
 
-                ["kind" => "unit", "test" => ['method'=> 'GET', 'url' => "api/entity/00001000-0000-4000-a000-000000000000",  'status' => 403] ],
-                ["kind" => "unit", "test" => ['method'=> 'GET', 'url' => "api/entity/00001000-0000-4000-a000-000000000000", 'token'=>'token_userForbidden',  'status' => 403] ],
-                ["kind" => "unit", "test" => ['method'=> 'GET', 'url' => "api/entity/00001000-0000-4000-a000-000000000000", 'token'=>'token_userActive',  'status' => 200, 'out'=>'getEntity'] ],
-                ["kind" => "unit", "test" => ['method'=> 'GET', 'url' => "api/entity/00001000-0000-4000-a000-000000000000", 'token'=>'token_userAdmin',  'status' => 200, 'out'=>'getEntity'] ],
+                ["kind" => "unit", "test" => ['method'=> 'GET'   , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"                                           , 'status' => 403] ],
+                ["kind" => "unit", "test" => ['method'=> 'GET'   , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userForbidden', 'status' => 403] ],
+                ["kind" => "unit", "test" => ['method'=> 'GET'   , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userActive'   , 'status' => 200, 'out'=>'getEntity'] ],
+                ["kind" => "unit", "test" => ['method'=> 'GET'   , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userAdmin'    , 'status' => 200, 'out'=>'getEntity'] ],
 
-                ["kind" => "unit", "test" => ['method'=> 'POST', 'url' => "api/entity/estate",  'status' => 401] ],
+                ["kind" => "unit", "test" => ['method'=> 'POST'  , 'url' => "api/entity/estate"                                                                         , 'status' => 401] ],
+                ["kind" => "unit", "test" => ['method'=> 'POST'  , 'url' => "api/entity/estate"                                         , 'token'=>'token_userActive'   , 'status' => 400, 'out'=>'postedMissingFieldEstate'   , 'in'=>'postMissingFieldEstate'] ],
+                ["kind" => "unit", "test" => ['method'=> 'POST'  , 'url' => "api/entity/estate"                                         , 'token'=>'token_userActive'   , 'status' => 400, 'out'=>'postedExtraFieldEstate'     , 'in'=>'postExtraFieldEstate'] ],
+                ["kind" => "unit", "test" => ['method'=> 'POST'  , 'url' => "api/entity/estate"                                         , 'token'=>'token_userActive'   , 'status' => 400, 'out'=>'postedInvalidEstate'        , 'in'=>'postInvalidEstate'] ],
+                ["kind" => "unit", "test" => ['method'=> 'POST'  , 'url' => "api/entity/estate"                                         , 'token'=>'token_userActive'   , 'status' => 409, 'out'=>'postedValidEstate'          , 'in'=>'postValidEstate'] ],
 
-                ["kind" => "unit", "test" => ['method'=> 'PATCH', 'url' => "api/entity/00001000-0000-4000-a000-000000000000",  'status' => 401] ],
-                ["kind" => "unit", "test" => ['method'=> 'PATCH', 'url' => "api/entity/00001000-0000-4000-a000-000000000000", 'token'=>'token_userForbidden',  'status' => 403] ],
+                ["kind" => "unit", "test" => ['method'=> 'PATCH' , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"                                           , 'status' => 401] ],
+                ["kind" => "unit", "test" => ['method'=> 'PATCH' , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userForbidden', 'status' => 403] ],
+                ["kind" => "unit", "test" => ['method'=> 'PATCH' , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userActive'   , 'status' => 400, 'out'=>'patchedExtraFieldEstate'    , 'in'=>'patchExtraFieldEstate'] ],
+                ["kind" => "unit", "test" => ['method'=> 'PATCH' , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userActive'   , 'status' => 400, 'out'=>'patchedInvalidEstate'       , 'in'=>'patchInvalidEstate'] ],
+                ["kind" => "unit", "test" => ['method'=> 'PATCH' , 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userActive'   , 'status' => 409, 'out'=>'patchedValidPostedEstate'   , 'in'=>'patchValidPostedEstate'] ],
+                ["kind" => "unit", "test" => ['method'=> 'PATCH' , 'url' => "api/entity/00003000-0000-4000-a000-000000000000"           , 'token'=>'token_userActive'   , 'status' => 409, 'out'=>'patchedValidPublishedEstate', 'in'=>'patchValidPublishedEstate'] ],
 
-                ["kind" => "unit", "test" => ['method'=> 'DELETE', 'url' => "api/entity/00001000-0000-4000-a000-000000000000",  'status' => 401] ],
-                ["kind" => "unit", "test" => ['method'=> 'DELETE', 'url' => "api/entity/00001000-0000-4000-a000-000000000000", 'token'=>'token_userForbidden',  'status' => 403] ],
-                ["kind" => "unit", "test" => ['method'=> 'DELETE', 'url' => "api/entity/00001000-0000-4000-a000-000000000000", 'token'=>'token_userActive',  'status' => 204] ],
-                ["kind" => "unit", "test" => ['method'=> 'DELETE', 'url' => "api/entity/00001000-0000-4000-a000-000000000000", 'token'=>'token_userAdmin',  'status' => 204] ],
+                ["kind" => "unit", "test" => ['method'=> 'DELETE', 'url' => "api/entity/00001000-0000-4000-a000-000000000000"                                           , 'status' => 401] ],
+                ["kind" => "unit", "test" => ['method'=> 'DELETE', 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userForbidden', 'status' => 403] ],
+                ["kind" => "unit", "test" => ['method'=> 'DELETE', 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userActive'   , 'status' => 204] ],
+                ["kind" => "unit", "test" => ['method'=> 'DELETE', 'url' => "api/entity/00001000-0000-4000-a000-000000000000"           , 'token'=>'token_userAdmin'    , 'status' => 204] ],
 
             ];
     }
