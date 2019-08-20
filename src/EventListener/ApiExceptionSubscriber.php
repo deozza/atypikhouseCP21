787062baac543b826a4538e222afe9196abf8323
pakiaka <antoine.megevant@gmail.com>
2019-08-20 10:15:55 +0200

    getRoles added in Authenticator (#45)
    * Added 1 test Password
    
    * Finished Password forgotten tests
    
    * Fix link
    
    * Fix json link Password
    
    * Fix again
    
    * GetRoles added to token in Authenticator

--- src/EventListener/ApiExceptionSubscriber.php
+++ src/EventListener/ApiExceptionSubscriber.php
@@ -15,7 +15,7 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            //KernelEvents::EXCEPTION => 'onKernelException'
+            KernelEvents::EXCEPTION => 'onKernelException'
         );
     }
 
