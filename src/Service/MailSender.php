a407e9bc450b35270e35939b10a07d814c86f72a
Edenn <deozza@gmail.com>
2019-09-23 12:29:24 +0200

    removed static mailer

--- src/Service/MailSender.php
+++ src/Service/MailSender.php
@@ -39,7 +39,6 @@ class MailSender
     private function sendEmail(string $templateName, array $context, string $subject, string $toEmail)
     {
         $message = (new \Swift_Message($subject))
-            ->setFrom('deozza@gmail.com')
             ->setTo($toEmail)
             ->setBody(
                 $this->twig->render($templateName, $context)
